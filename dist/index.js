6a9a4fdcd178189b8cc3b4535fda86a95b83ac4f
Sander Jochems <sanderjochems@hotmail.nl>
2019-11-08 14:19:41 +0100

    Update index.js
    Add `target_commitish` to create release

--- dist/index.js
+++ dist/index.js
@@ -7861,6 +7861,7 @@ async function run() {
     // This removes the 'refs/tags' portion of the string, i.e. from 'refs/tags/v1.10.15' to 'v1.10.15'
     const tag = tagName.replace('refs/tags/', '');
     const releaseName = core.getInput('release_name', { required: true }).replace('refs/tags/', '');
+    const target_commitish = core.getInput('target_commitish', { required: false });
     const draft = core.getInput('draft', { required: false }) === 'true';
     const prerelease = core.getInput('prerelease', { required: false }) === 'true';
 
@@ -7872,6 +7873,7 @@ async function run() {
       repo,
       tag_name: tag,
       name: releaseName,
+      target_commitish,
       draft,
       prerelease
     });
