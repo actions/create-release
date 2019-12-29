2b67105e25319af11da2be00be8cc29085afdeb7
fleskesvor <eivindkn@gmail.com>
2019-12-29 09:06:12 +0100

    Tag and release on a specific commitish

--- src/create-release.js
+++ src/create-release.js
@@ -18,6 +18,7 @@ async function run() {
     const body = core.getInput('body', { required: false });
     const draft = core.getInput('draft', { required: false }) === 'true';
     const prerelease = core.getInput('prerelease', { required: false }) === 'true';
+    const commitish = core.getInput('commitish', { required: false }) || context.sha;
 
     // Create a release
     // API Documentation: https://developer.github.com/v3/repos/releases/#create-a-release
@@ -29,7 +30,8 @@ async function run() {
       name: releaseName,
       body,
       draft,
-      prerelease
+      prerelease,
+      target_commitish: commitish
     });
 
     // Get the ID, html_url, and upload URL for the created Release from the response
