64c9a1fc5f8bb3bd806fff9fbf32ac9f6e25310e
Jason Karns <jason.karns@gmail.com>
2020-03-26 22:40:40 -0400

    Default token from github.token

--- src/create-release.js
+++ src/create-release.js
@@ -4,7 +4,7 @@ const { GitHub, context } = require('@actions/github');
 async function run() {
   try {
     // Get authenticated GitHub client (Ocktokit): https://github.com/actions/toolkit/tree/master/packages/github#usage
-    const github = new GitHub(process.env.GITHUB_TOKEN);
+    const github = new GitHub(process.env.GITHUB_TOKEN || core.getInput('token'));
 
     // Get owner and repo from context of payload that triggered the action
     const { owner, repo } = context.repo;
