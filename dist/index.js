e282b3bf74362aac9f4d2394f9f26c034b64aa7f
Matt Sergeant <helpme+github@gmail.com>
2020-09-02 16:42:52 -0400

    Attempt to do the version number boogaloo

--- dist/index.js
+++ dist/index.js
@@ -8783,6 +8783,14 @@ const core = __webpack_require__(470);
 const { GitHub, context } = __webpack_require__(469);
 const fs = __webpack_require__(747);
 
+const MAX_SUB_VERSION = 100;
+async function getCurrentRelease(github, owner, repo, tag, version = 0) {
+  if (version >= MAX_SUB_VERSION) return null;
+  const currentRelease = (await github.repos.getReleaseByTag({ owner, repo, tag: `${tag}.${version}` })).data;
+  if (currentRelease) return currentRelease;
+  return getCurrentRelease(github, owner, repo, tag, version + 1);
+}
+
 async function run() {
   try {
     // Get authenticated GitHub client (Ocktokit): https://github.com/actions/toolkit/tree/master/packages/github#usage
@@ -8813,10 +8821,11 @@ async function run() {
     }
 
     let createReleaseResponse;
+    let v = 0;
     const params = {
       owner,
       repo,
-      tag_name: tag,
+      tag_name: `${tag}.${v}`,
       name: releaseName,
       body: bodyFileContent || body,
       draft,
@@ -8824,27 +8833,29 @@ async function run() {
       target_commitish: commitish
     };
     try {
-        console.log("Trying to release with params:", params);
-        // Create a release
-        // API Documentation: https://developer.github.com/v3/repos/releases/#create-a-release
-        // Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-create-release
-        createReleaseResponse = await github.repos.createRelease(params);
-    }
-    catch (e) {
-        console.log(`Release failed: ${e.message} - trying to update`);
-        // Failed to create - maybe it already exists...
-        // Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-get-release-by-tag
-        const currentRelease = (await github.repos.getReleaseByTag({owner, repo, tag})).data;
-        console.log(`Current release:`, currentRelease);
-        if (!currentRelease) throw e;
+      console.log('Trying to release with params:', params);
+      // Create a release
+      // API Documentation: https://developer.github.com/v3/repos/releases/#create-a-release
+      // Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-create-release
+      createReleaseResponse = await github.repos.createRelease(params);
+    } catch (e) {
+      console.log(`Release failed: ${e.message} - trying to update`);
+      // Failed to create - maybe it already exists...
+      // Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-get-release-by-tag
+      const currentRelease = await getCurrentRelease(github, owner, repo, tag);
+      if (!currentRelease) throw e;
+      console.log(`Current release:`, currentRelease);
+      const m = /\.(\d+)$/.exec(currentRelease.tag_name);
+      if (!m) {
+        throw new Error(`tag_name ${currentRelease.tag_name} doesn't match regexp`);
+      }
+      v = parseInt(m[1], 10) + 1;
 
-        console.log(`Deleting the tag`);
-        // Delete the tag
-        await github.git.deleteRef({ owner, repo, ref: `tags/${tag}` });
-        // Delete the release
-        await github.repos.deleteRelease({ owner, repo, release_id: currentRelease.id });
-        // and recreate
-        createReleaseResponse = await github.repos.createRelease(params);
+      // Delete the release
+      await github.repos.deleteRelease({ owner, repo, release_id: currentRelease.id });
+      params.tag_name = `${tag}.${v}`;
+      // and recreate
+      createReleaseResponse = await github.repos.createRelease(params);
     }
 
     console.log(`End result: `, createReleaseResponse);
